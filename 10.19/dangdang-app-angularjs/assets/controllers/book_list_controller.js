9cd09478a697294d695fc6bb20f2c5dae15f6942
TheDonkey <zhouguangwei@zhouguangweideMac-mini.local>
2016-10-20 09:36:57 +0800

    添加10.19项目

---
+++ 10.19/dangdang-app-angularjs/assets/controllers/book_list_controller.js
@@ -0,0 +1,8 @@
+
+app.controller('bookListController',['$scope','commonService','$routeParams',function($scope,c_service,$routeParams){
+     c_service.getData('$routeParams.id', function (res) {
+        console.dir(res);
+        $scope.data = res.data;
+    })
+    
+}])
