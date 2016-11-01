f757ee1e8ad330867001a6f4b4afbe273d392430
TheDonkey <zhouguangwei@zhouguangweideMac-mini.local>
2016-11-01 13:31:58 +0800

    修改图片路径

--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,8 +1,8 @@
 app.controller('bookController',['$scope',function($scope){
     $scope.bookTypes = [
-        {id:'ertong',name:'儿童',img:'../images/ertong.jpg',description:'儿童热卖新品'},
-        {id:'lishi',name:'历史',img:'../images/lishi.jpg',description:'历史热卖新品'},
-        {id:'dongman',name:'动漫',img:'../images/dongman.jpg',description:'动漫热卖新品'},
-        {id:'qingchunwenxue',name:'青春文学',img:'../images/qingchun.jpg',description:'青春热卖新品'},
+        {id:'ertong',name:'儿童',img:'./assets/images/ertong.jpg',description:'儿童热卖新品'},
+        {id:'lishi',name:'历史',img:'./assets/images/lishi.jpg',description:'历史热卖新品'},
+        {id:'dongman',name:'动漫',img:'./assets/images/dongman.jpg',description:'动漫热卖新品'},
+        {id:'qingchunwenxue',name:'青春文学',img:'./assets/images/qingchun.jpg',description:'青春热卖新品'},
     ]
 }])
