e39ff9b0229fd009bd4cbe32c442e6b0b1c423d1
TheDonkey <zhouguangwei@zhouguangweideMac-mini.local>
2016-11-01 12:03:05 +0800

    app.js修改当当的链接html

--- dangdang-app-angularjs/assets/app.js
+++ dangdang-app-angularjs/assets/app.js
@@ -10,11 +10,11 @@ app.config(['$routeProvider', function($routeProvider) {
           controller:''
         })
         .when('/book',{
-          templateUrl:'./assets/tpl/blog.html',
+          templateUrl:'./assets/tpl/book.html',
           controller:'bookController'
         })
         .when('/about_me',{
-          templateUrl:'./assets/tpl/book.html',
+          templateUrl:'./assets/tpl/about_me.html',
           controller:''
         })
         .when('/book_list/:id',{
