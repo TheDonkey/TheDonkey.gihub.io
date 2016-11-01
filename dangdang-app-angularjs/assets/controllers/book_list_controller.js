00fc4f6d20d9b3dca697844bd8d5502737827ce4
TheDonkey <zhouguangwei@zhouguangweideMac-mini.local>
2016-11-01 12:44:06 +0800

    修改 book_list_controller.js传递参数

--- dangdang-app-angularjs/assets/controllers/book_list_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_list_controller.js
@@ -1,6 +1,6 @@
 
 app.controller('bookListController',['$scope','commonService','$routeParams',function($scope,c_service,$routeParams){
-     c_service.getData('$routeParams.id', function (res) {
+     c_service.getData($routeParams.id, function (res) {
         console.dir(res);
         $scope.data = res.data;
         console.log($scope.data);
