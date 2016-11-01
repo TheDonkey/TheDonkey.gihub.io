13127c2c74282f03eadfe1b67ff758bac445ba90
TheDonkey <zhouguangwei@zhouguangweideMac-mini.local>
2016-11-01 16:15:41 +0800

    修改了展示

--- yamaxun-app-angularjs/assets/controllers/book_controller.js
+++ yamaxun-app-angularjs/assets/controllers/book_controller.js
@@ -1,8 +1,8 @@
 app.controller('bookController',['$scope',function($scope){
   $scope.bookTypes = [
-    {id:'ertong',name:'儿童',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'当当网童书热销榜'},
-    {id:'lishi',name:'历史',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'当当网历史书热销榜'},
-    {id:'dongman',name:'动漫',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'当当网热销榜'},
-    {id:'qingchunwenxue',name:'青春文学',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'当当网热销榜'}
+    {id:'ertong',name:'儿童',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'童书热销榜'},
+    {id:'lishi',name:'历史',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'历史书热销榜'},
+    {id:'dongman',name:'动漫',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'动漫热销榜'},
+    {id:'qingchunwenxue',name:'青春文学',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'青春热销榜'}
   ]
 }])
