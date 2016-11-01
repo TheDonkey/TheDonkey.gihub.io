705e547a1deb603ab294940590df7c1c7886384b
TheDonkey <zhouguangwei@zhouguangweideMac-mini.local>
2016-11-01 13:44:57 +0800

    添加图片

--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,8 +1,8 @@
 app.controller('bookController',['$scope',function($scope){
     $scope.bookTypes = [
-        {id:'ertong',name:'儿童',img:'./assets/images/ertong.jpg',description:'儿童热卖新品'},
-        {id:'lishi',name:'历史',img:'./assets/images/lishi.jpg',description:'历史热卖新品'},
-        {id:'dongman',name:'动漫',img:'./assets/images/dongman.jpg',description:'动漫热卖新品'},
-        {id:'qingchunwenxue',name:'青春文学',img:'./assets/images/qingchun.jpg',description:'青春热卖新品'},
+        {id:'ertong',name:'儿童',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'儿童热卖新品'},
+        {id:'lishi',name:'历史',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'历史热卖新品'},
+        {id:'dongman',name:'动漫',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'动漫热卖新品'},
+        {id:'qingchunwenxue',name:'青春文学',img:'http://img3x8.ddimg.cn/97/8/23812468-1_l_1.jpg',description:'青春热卖新品'},
     ]
 }])
